c4e79dcf2ac12a6403b3b0a83f6afa67b1cc1e3d
sharrie <sharrie.s.chen@gmail.com>
2013-05-19 21:39:46 -0700

    design extended to main site

--- js/functions.js
+++ js/functions.js
@@ -3,67 +3,16 @@
 
 
 /* trigger when page is ready */
-$(document).ready(function (){
-   
-      $(".twitter").simpletip({
-         content: 'twitter',
-         showEffect: 'slide',
-         hideEffect: 'fade',
-         fixed: true,
-         position: ["0","58"]
-      });   
-      $(".instagram").simpletip({
-         content: 'instagram',
-         showEffect: 'slide',
-         hideEffect: 'fade',
-         fixed: true,
-         position: ["0","58"]
-      });   
-      $(".github").simpletip({
-         content: 'github',
-         showEffect: 'slide',
-         hideEffect: 'fade',
-         fixed: true,
-         position: ["0","58"]
-      });
-      
-      $(".flickr").simpletip({
-         content: 'flickr',
-         showEffect: 'slide',
-         hideEffect: 'fade',
-         fixed: true,
-         position: ["0","58"]
-      });   
-
-      $(".tumblr").simpletip({
-         content: 'tumblr',
-         showEffect: 'slide',
-         hideEffect: 'fade',
-         fixed: true,
-         position: ["0","58"]
-      });   
-      $(".linkedin").simpletip({
-         content: 'linkedin',
-         showEffect: 'slide',
-         hideEffect: 'fade',
-         fixed: true,
-         position: ["0","58"]
-      }); 
-      $(".facebook").simpletip({
-         content: 'facebook',
-         showEffect: 'slide',
-         hideEffect: 'fade',
-         fixed: true,
-         position: ["0","58"]
-      });    
-      $(".home").simpletip({
-         content: 'home',
+$(document).ready(function (){ 
+   /*
+      $(".teatime").simpletip({
+         content: 'hello',
          showEffect: 'slide',
          hideEffect: 'fade',
          fixed: true,
          position: ["-68","0"]
       });
-
+   */
 });
 
 
