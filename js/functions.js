eda16896a269193082fb12e6355a0a2c9f782b88
sharrie <sharrie.s.chen@gmail.com>
2014-02-15 18:17:28 -0800

    vertical alignment

--- js/functions.js
+++ js/functions.js
@@ -4,15 +4,17 @@
 
 /* trigger when page is ready */
 $(document).ready(function (){ 
-   
-      $("").simpletip({
-         content: 'UI/UX DESIGNER',
-         showEffect: 'fade',
-         hideEffect: 'fade',
-         showTime: 1200,
-         hideTime: 1200,
-         fixed: true,
-         position: ["-100","0"]
-      });
+
+	$(window).resize(function(){
+
+	    $('.wrapper').css({
+	        position:'absolute',
+	        left: ($(window).width() - $('.wrapper').outerWidth())/2,
+	        top: ($(window).height() - $('.wrapper').outerHeight())/2
+	    });
+
+	});
+
+	$(window).resize();
 
 });
