97175bc8018e4e439082dfbcbdd647b778feb579
sharrie <sharrie.s.chen@gmail.com>
2014-05-26 16:37:16 -0700

    2column restructure

--- js/functions.js
+++ js/functions.js
@@ -5,16 +5,5 @@
 /* trigger when page is ready */
 $(document).ready(function (){ 
 
-	$(window).resize(function(){
-
-	    $('.wrapper').css({
-	        position:'absolute',
-	        left: ($(window).width() - $('.wrapper').outerWidth())/2,
-	        top: ($(window).height() - $('.wrapper').outerHeight())/2
-	    });
-
-	});
-
-	$(window).resize();
 
 });
